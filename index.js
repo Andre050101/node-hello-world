20dfa505f0723c235ed00d987aab155a1b5ea22c
Andre050101 <agiancristiano90@gmail.com>
2024-11-28 17:38:39 +0100

    another solution added (with nullish coaeliscing operator '??')

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//Solution 1 (normal if-else control) 
 const parolaPassata = process.argv[2];
 if (parolaPassata === undefined || parolaPassata === null)
     console.log("Hello guest");
 else
     console.log(`Hello ${parolaPassata}`);
 
+//Solution 2 (with nullish coalescing operator (??))
+console.log(`Hello ${parolaPassata ?? "guest"}`);
